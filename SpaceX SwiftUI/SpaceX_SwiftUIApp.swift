0df86bb4f5a2fdee62f753c72a5378a8f0b39e6b
Joel Sene <joel.sene@e-unicred.com.br>
2021-12-09 15:49:32 -0300

    Initial Commit

---
+++ SpaceX SwiftUI/SpaceX_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceX_SwiftUIApp.swift
+//  SpaceX SwiftUI
+//
+//  Created by Joel Sene on 09/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SpaceX_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
